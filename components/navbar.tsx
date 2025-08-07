668d8237f115f5e133d383232424a4d7a0d769a2
Abhay <abhaykumargond9164@gmail.com>
2025-08-07 10:18:49 +0530

    Update navbar and page styles

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,7 +15,8 @@ import { link as linkStyles } from "@nextui-org/theme";
 import NextLink from "next/link";
 import clsx from "clsx";
 
-import { siteConfig } from "@/config/site";
+import { siteConfig } from "../config/site";
+
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
   TwitterIcon,
