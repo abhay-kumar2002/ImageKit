5bb13b122e476ab032686c963ec3db7b2856c85b
Abhay <abhaykumargond9164@gmail.com>
2025-08-07 10:16:39 +0530

    Add siteConfig file required by navbar

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+// config/site.ts
+export const siteConfig = {
+  navItems: [
+    { label: "Home", href: "/" },
+    { label: "Features", href: "/features" },
+    { label: "Pricing", href: "/pricing" },
+  ],
+  navMenuItems: [
+    { label: "Home" },
+    { label: "Features" },
+    { label: "Logout" },
+  ],
+  links: {
+    sponsor: "/login",
+    github: "https://github.com/abhay-kumar2002",
+  },
+};
